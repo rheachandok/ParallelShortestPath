b8bd1c1c57da7e3c13be353d209636b2401d3e53
Rhea Chandok <rhea1298@gmail.com>
2025-04-11 12:42:12 -0400

    Changes in Johnsons

--- parallel/johnsons_parallel.cpp
+++ parallel/johnsons_parallel.cpp
@@ -1,101 +1,95 @@
 #include <iostream>
 #include <vector>
 #include <tuple>
+#include <queue>
 #include <limits>
 #include <omp.h>
-#include "../algorithm_fns.h"
 
 using namespace std;
 
 #define INF numeric_limits<int>::max()
 
-vector<vector<int>> johnsonsParallel(int nodes, const vector<tuple<int, int, int>> &edges, int numThreads) {
+// Standard Dijkstra using priority queue
+vector<int> dijkstra(int src, const vector<vector<pair<int, int>>>& reweightedGraph) {
+    int nodes = reweightedGraph.size();
+    vector<int> dist(nodes, INF);
+    dist[src] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    pq.push({0, src});
+
+    while (!pq.empty()) {
+        auto [d, u] = pq.top(); pq.pop();
+        if (d > dist[u]) continue;
+
+        for (auto &[v, weight] : reweightedGraph[u]) {
+            if (dist[u] + weight < dist[v]) {
+                dist[v] = dist[u] + weight;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+    return dist;
+}
+
+vector<vector<int>> johnsonsParallel(int nodes, const vector<tuple<int, int, int>>& edges, int numThreads) {
     vector<int> h(nodes, INF);
     h[nodes - 1] = 0;
 
-    // Parallel Bellman-Ford to find potential values (h)
-    for (int i = 0; i < nodes - 1; i++) {
+    // Step 1: Run Bellman-Ford to compute h[]
+    for (int i = 0; i < nodes - 1; ++i) {
         vector<int> new_h = h;
 
         #pragma omp parallel for num_threads(numThreads)
-        for (int j = 0; j < edges.size(); j++) {
-            int u, v, weight;
-            tie(u, v, weight) = edges[j];
-
-            if (h[u] != INF && h[u] + weight < new_h[v]) {
+        for (int j = 0; j < edges.size(); ++j) {
+            int u, v, w;
+            tie(u, v, w) = edges[j];
+            if (h[u] != INF) {
+                int candidate = h[u] + w;
                 #pragma omp critical
                 {
-                    if (h[u] + weight < new_h[v]) {
-                        new_h[v] = h[u] + weight;
-                    }
-                }
-            }
-	}
-
-	h = new_h;
-    }
-
-    // Reweight edges
-    vector<vector<int>> dist(nodes, vector<int>(nodes, INF));
-    for (int i = 0; i < nodes; i++) dist[i][i] = 0;
-
-    #pragma omp parallel for num_threads(numThreads)
-    for (int j = 0; j < edges.size(); j++) {
-        int u, v, weight;
-        tie(u, v, weight) = edges[j];
-        dist[u][v] = weight + h[u] - h[v];
-    }
-    // Run Dijkstra for each node in parallel
-    #pragma omp parallel for num_threads(numThreads)
-    for (int src = 0; src < nodes; src++) {
-        vector<int> minDist(nodes, INF);
-        minDist[src] = 0;
-
-        vector<bool> visited(nodes, false);
-        for (int i = 0; i < nodes; i++) {
-            // Find the node with the smallest distance that hasn't been visited
-            int u = -1;
-            for (int j = 0; j < nodes; j++) {
-                if (!visited[j] && (u == -1 || minDist[j] < minDist[u])) {
-                    u = j;
-                }
-            }
-
-            if (minDist[u] == INF) break;
-            visited[u] = true;
-
-            // Parallelize the relaxation step for each edge
-            #pragma omp parallel for num_threads(numThreads)
-            for (int v = 0; v < nodes; v++) {
-                if (dist[u][v] != INF && minDist[u] + dist[u][v] < minDist[v]) {
-                    #pragma omp critical
-                    {
-                        if (minDist[u] + dist[u][v] < minDist[v]) {
-                            minDist[v] = minDist[u] + dist[u][v];
-                        }
+                    if (candidate < new_h[v]) {
+                        new_h[v] = candidate;
                     }
                 }
             }
         }
 
-        // Update distances for the source node
-        #pragma omp parallel for num_threads(numThreads)
-        for (int i = 0; i < nodes; i++) {
-            dist[src][i] = (minDist[i] == INF) ? INF : minDist[i] - h[src] + h[i];
+        h = new_h;
+    }
+
+    // Step 2: Reweight edges and build adjacency list
+    vector<vector<pair<int, int>>> reweightedGraph(nodes);
+    for (const auto& [u, v, w] : edges) {
+        if (h[u] != INF && h[v] != INF) {
+            int rw = w + h[u] - h[v];
+            reweightedGraph[u].emplace_back(v, rw);
         }
     }
 
-    // Printing the result matrix
+    // Step 3: Run Dijkstra for each node in parallel
+    vector<vector<int>> result(nodes, vector<int>(nodes, INF));
+
+    #pragma omp parallel for num_threads(numThreads)
+    for (int src = 0; src < nodes; ++src) {
+        vector<int> minDist = dijkstra(src, reweightedGraph);
+        for (int dest = 0; dest < nodes; ++dest) {
+            if (minDist[dest] != INF && h[src] != INF && h[dest] != INF) {
+                result[src][dest] = minDist[dest] - h[src] + h[dest];
+            }
+        }
+    }
+
+    // Optional: Print result matrix
     cout << "Johnson’s Algorithm (Parallel) - Shortest Distances:\n";
-    for (int i = 0; i < nodes; i++) {
-        for (int j = 0; j < nodes; j++) {
-            if (dist[i][j] == INF)
+    for (int i = 0; i < nodes; ++i) {
+        for (int j = 0; j < nodes; ++j) {
+            if (result[i][j] == INF)
                 cout << "INF ";
             else
-                cout << dist[i][j] << " ";
+                cout << result[i][j] << " ";
         }
-        cout << endl;
+        cout << "\n";
     }
 
-    return dist;
+    return result;
 }
